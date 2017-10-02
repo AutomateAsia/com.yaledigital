fbe0f2a01cc695d987d411093fef8418ecae2937
limkopi78 <32301049+limkopi78@users.noreply.github.com>
2017-10-02 16:00:17 +0800

    Update driver.js

--- drivers/yalelock_v1/driver.js
+++ drivers/yalelock_v1/driver.js
@@ -21,23 +21,6 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 
 			}
 		},
-		'alarm_keypad_unlocked': {
-			'command_class'				: 'COMMAND_CLASS_ALARM',
-			//'command_get'				: 'ALARM_GET',
-			'command_report'			: 'ALARM_REPORT',
-			'command_report_parser': (report, node) => {
-				if (report.hasOwnProperty("Alarm Type")) {
-					if (report['Alarm Type'] == '19' && report.hasOwnProperty("Alarm Level")) {
-						const token = {
-							"userid": report['Alarm Level']
-						};
-						Homey.manager('flow').triggerDevice('keypad_unlocked', token, null, node.device_data);
-						//console.log('alarm_keypad_unlocked, userid is ' + report['Alarm Level']);
-					};
-					return null;
-				}
-			}
-		}/*,
 		'alarm_manual_unlocked': {
 			'command_class'				: 'COMMAND_CLASS_ALARM',
 			//'command_get'				: 'ALARM_GET',
@@ -47,23 +30,6 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 					if (report['Alarm Type'] == '22') {
 						Homey.manager('flow').triggerDevice('manual_unlocked', null, null, node.device_data);
 						//console.log('alarm_manual_unlocked');
-					};
-					return null;
-				}
-			}
-		},
-		'alarm_keypad_locked': {
-			'command_class'				: 'COMMAND_CLASS_ALARM',
-			//'command_get'				: 'ALARM_GET',
-			'command_report'			: 'ALARM_REPORT',
-			'command_report_parser': (report, node) => {
-				if (report.hasOwnProperty("Alarm Type")) {
-					if (report['Alarm Type'] == '18' && report.hasOwnProperty("Alarm Level")) {
-						const token = {
-							"userid": report['Alarm Level']
-						};
-						Homey.manager('flow').triggerDevice('keypad_locked', token, null, node.device_data);
-						//console.log('alarm_keypad_locked, userid is ' + report['Alarm Level']);
 					};
 					return null;
 				}
