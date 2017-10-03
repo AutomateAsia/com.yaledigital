3b11646ff5f53a021421fe59d3af0003f02c431a
limkopi78 <32301049+limkopi78@users.noreply.github.com>
2017-10-03 16:24:35 +0800

    Update driver.js

--- drivers/yalelock_v1/driver.js
+++ drivers/yalelock_v1/driver.js
@@ -21,7 +21,7 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 
 			}
 		},
-		'alarm_keypad_unlocked': {
+		'alarm_manual_unlocked': {
 			'command_class'				: 'COMMAND_CLASS_ALARM',
 			'command_get'				: 'ALARM_GET',
 			'command_report'			: 'ALARM_REPORT',
